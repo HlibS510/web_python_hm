3413cc675794973e0aef9f1e423629a1723808d7
HlibS510 <hlibsorokin@gmail.com>
2026-02-20 13:23:45 +0100

    login fix

--- blog/login_db.py
+++ blog/login_db.py
@@ -13,14 +13,15 @@ def init_db():
         db.commit()
 
 def create_new_account(name, password):
-    try:
-        with get_db() as db:
-            db.execute("INSERT INTO logins (name, password) VALUES (?, ?)", (name, password))
-            db.commit()
-    except:
-        print("Error")
+    with get_db() as db:
+        user = db.execute("INSERT INTO IF NOT EXISTS logins (name, password) VALUES (?, ?)", (name, password))
+        db.commit()
+    if user:
+        return True
+    else:
+        return False
 
-def login(name, password):
+def login_user(name, password):
     with get_db() as db:
         user = db.execute("SELECT * FROM logins WHERE name = ? AND password = ?",(name, password)).fetchone()
         db.commit()
--- blog/main.py
+++ blog/main.py
@@ -19,31 +19,38 @@ def index():
     return render_template('index.html', sections=sections, results=results, input=input, name=display_name)
 
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    session["loggined"] = False
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    session["login"] = False
     if request.method == "POST":
         name = request.form.get('name')
         password = request.form.get('password')
 
-        if session["loggined"] == False:
-            try:
-                if login(name, password):
-                    print("Logged in")
-                    session["login_name"] = name
-                    session["loggined"] = True
-                    return redirect(url_for("index"))
-                else:
-                    create_new_account(name, password)
-                    print("Login failed")
-                    session["login_name"] = name
-                    session["loggined"] = True
-                    return redirect(url_for("index"))
-            except:
-                print("Error")
+        if login_user(name, password):
+            print("Logged in")
+            session["login_name"] = name
+            session["login"] = True
+            return redirect(url_for("index"))
         else:
-            print("logged in")
-    return render_template("login.html")
+            error = 'Incorrect password or username.'
+    return render_template("login.html", error=error)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    session["login"] = False
+    if request.method == "POST":
+        name = request.form.get('name')
+        password = request.form.get('password')
+
+        if create_new_account(name, password):
+            session["login_name"] = name
+            session["login"] = True
+            return redirect(url_for("index"))
+        else:
+            error = 'Username is already used, or something went wrong'
+
+    return render_template("register.html")
 
 @app.route('/<section_slug>')
 def section_page(section_slug):
@@ -69,15 +76,19 @@ def like_post(post_id):
 def add_post():
     display_name = session.get("login_name", "Login")
     sections = get_blog_sections()
+    login_name = session.get("login_name")
 
-    if request.method == "POST":
-        name = session["login_name"]
-        text = request.form["text"]
-        image = request.form["image"]
-        section_id = int(request.form["section"])
-        create_new_post(name, text, image, section_id)
-        section = get_section_by_id(section_id)
-        return redirect(url_for("section_page", sections=sections, section_slug=section["slug"]))
+    if login_name:
+        if request.method == "POST":
+            name = session["login_name"]
+            text = request.form["text"]
+            image = request.form["image"]
+            section_id = int(request.form["section"])
+            create_new_post(name, text, image, section_id)
+            section = get_section_by_id(section_id)
+            return redirect(url_for("section_page", sections=sections, section_slug=section["slug"]))
+    else:
+        return redirect(url_for("register"))
 
     return render_template("add_post.html", sections=sections, name=display_name)
 
